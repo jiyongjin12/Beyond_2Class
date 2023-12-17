0c8a18f1e944505a7e3e0682d175b7c0f458ab68
jiyongjin <l1110hj@gmail.com>
2023-12-17 14:22:16 +0900

    Asset

---
+++ Assets/Script/Shild.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shild : MonoBehaviour
+{
+    public GameObject pl;
+
+    private void Update()
+    {
+
+    }
+}
+
